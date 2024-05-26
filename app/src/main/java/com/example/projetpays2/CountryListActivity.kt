f6627009a2535fc84c8b8eaf5eff1da33e52a32d
Mathieuepf <mathieu.roubaud@epfedu.fr>
2024-05-26 18:17:18 +0200

    Fondations du projet (activity + views) projet compile

---
+++ app/src/main/java/com/example/projetpays2/CountryListActivity.kt
@@ -0,0 +1,28 @@
+package com.example.projetpays2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class CountryListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_country_list)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val listCountryRecView =
+            findViewById<RecyclerView>(R.id.list_countries_recyclerView)
+
+        listCountryRecView.layoutManager =
+            LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false)
+    }
+}
