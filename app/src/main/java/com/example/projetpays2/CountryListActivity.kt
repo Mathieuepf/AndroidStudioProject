66f2c52ccfc90b934614540fa7c54678bfe1c41b
Mathieuepf <mathieu.roubaud@epfedu.fr>
2024-06-05 17:52:36 +0200

    Creation du menu pour implementer l'API mais menu ne se charge pas (fonction onCreateOptionsMenu ne s'execute pas)

--- app/src/main/java/com/example/projetpays2/CountryListActivity.kt
+++ app/src/main/java/com/example/projetpays2/CountryListActivity.kt
@@ -3,18 +3,22 @@ package com.example.projetpays2
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toolbar
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 class CountryListActivity : AppCompatActivity() {
+
+    lateinit var listCountryRecView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -25,7 +29,9 @@ class CountryListActivity : AppCompatActivity() {
             insets
         }
 
-        val listCountryRecView =
+        System.out.println("On est là")
+
+        listCountryRecView =
             findViewById<RecyclerView>(R.id.list_countries_recyclerView)
 
         listCountryRecView.layoutManager =
@@ -34,8 +40,25 @@ class CountryListActivity : AppCompatActivity() {
 
         val countries = Country.generateClountry(10)
         listCountryRecView.adapter = CountryAdapter(countries)
+
+//        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+//        setSupportActionBar(toolbar)
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        System.out.println("menu créé")
+        menuInflater.inflate(R.menu.list_country, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.button_research_menu -> {
+                displayCountries()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     private fun displayCountries() {
         val httpLoggingInterceptor = HttpLoggingInterceptor().apply {
@@ -53,5 +76,16 @@ class CountryListActivity : AppCompatActivity() {
             .build()
 
         val countryService = retrofit.create(CountryService::class.java)
+
+        runBlocking {
+            val countries = countryService.getCountries(10)
+            val countryList = countries.results.map {
+                Country(
+                    it.name, it.flag
+                )
+            }
+            val adapter = CountryAdapter(countryList)
+            listCountryRecView.adapter = adapter
+        }
     }
 }
